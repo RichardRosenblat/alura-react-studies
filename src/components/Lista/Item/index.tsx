256216d00cbcd612bd9a8322d0d1c6dfb5b9b711
richard rosenblat <rosenblatr@gmail.com>
2022-09-03 15:31:37 -0300

    Select task

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,17 +1,35 @@
 import { ITarefa } from "../../../types/tarefa";
 import style from "../Lista.module.scss";
 
+interface props extends ITarefa {
+	selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
 export default function item({
 	tarefa,
 	tempo,
 	completado,
 	id,
 	selecionado,
-}: ITarefa) {
-	console.log("item atual",arguments);
-	
+	selecionaTarefa,
+}: props) {
+	console.log("item atual", arguments);
+
 	return (
-		<li className={style.item}>
+		<li
+			className={`${style.item} ${
+				selecionado ? style.itemSelecionado : ""
+			}`}
+			onClick={() =>
+				selecionaTarefa({
+					tarefa,
+					tempo,
+					completado,
+					id,
+					selecionado,
+				})
+			}
+		>
 			<h3>{tarefa}</h3>
 			<span>{tempo}</span>
 		</li>
